d38ea9337144c05d5abbdb36fc5badd76d38324c
Colin Nielsen <colinrnielsen@gmail.com>
2023-06-27 11:43:49 -0600

    init

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        Counter counter = new Counter();
+        counter.setNumber(42);
+        vm.stopBroadcast();
     }
 }
